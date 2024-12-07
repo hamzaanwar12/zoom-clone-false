7b1ce5ca371e4fc194c80f23ab1590d18a2cdcff
Hamza Anwar <147822744+hamzaanwar12@users.noreply.github.com>
2024-12-07 13:28:36 +0500

    End Call Fixed

--- components/EndCallButton.tsx
+++ components/EndCallButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import Button from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 import { useCall, useCallStateHooks } from "@stream-io/video-react-sdk";
 import { useRouter } from "next/navigation";
 export default function EndCallButton() {
@@ -14,15 +14,16 @@ export default function EndCallButton() {
     call?.state.createdBy &&
     localParticpant.userId == call.state.createdBy.id;
 
-  const handleClick = async () => {
-    await call.endCall();
-    router.push("/");
-  };
-
   if (!isMeetingOwner) return null;
 
   return (
-    <Button className="bg-red-500" onClick={handleClick}>
+    <Button
+      className="bg-red-500"
+      onClick={async () => {
+        await call.endCall();
+        router.push("/");
+      }}
+    >
       End Call
     </Button>
   );
